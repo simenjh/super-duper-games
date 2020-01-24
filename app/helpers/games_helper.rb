14f825a1da8765e1e902da0972392cb55a1fba48
Simen Janbu Høgseth <simenjh1@gmail.com>
2020-01-24 10:56:08 +0100

    Refactor views

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,2 @@
 module GamesHelper
-  def play_game(game)
-    random_score = Random.rand(1) * 1000
-    @playthrough = Playthrough.new(score: random_score, user_id: current_user.id, game_id: game.id)
-    byebug
-  end
 end
